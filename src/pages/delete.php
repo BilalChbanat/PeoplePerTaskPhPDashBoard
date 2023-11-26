0d19fd5cd6ee9188471faad74be65ccad472f196
BilalChbanat <bilal.chbanat2003@gmail.com>
2023-11-26 16:48:55 +0100

    delete is done

--- src/pages/delete.php
+++ src/pages/delete.php
@@ -1,3 +1,10 @@
 <?php
 include 'dbh.inc.php';
-?>
+
+if (isset($_GET['id'])) {
+    $id = $_GET['id'];
+
+    $sqldelete = "DELETE FROM user WHERE id_user = $id";
+    mysqli_query($conn, $sqldelete);
+    header("location: dashclient.php");
+}
