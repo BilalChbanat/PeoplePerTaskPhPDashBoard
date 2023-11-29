90dcd6f596153e8fc7741d5f77aead344d5bd595
BilalChbanat <bilal.chbanat2003@gmail.com>
2023-11-29 15:20:34 +0100

    DRY header.php nav.php

---
+++ nav.php
@@ -0,0 +1,90 @@
+<div class="mb-[15%]">
+    <img src="../../images/Link_logo.svg.svg" alt="logo">
+</div>
+<ul class=" ml-[2%] mb-[10%] text-lg">
+    <li
+        class="flex p-3 mb-15 w-3/4 h-14 transition-transform duration-200 ease-out transform-colors dark:hover:bg-purple-700  hover:scale-110 rounded mb-[10%]">
+        <script src="https://cdn.lordicon.com/lordicon-1.4.0.js"></script>
+        <lord-icon src="https://cdn.lordicon.com/zrtfxghu.json" trigger="loop" colors="primary:#6366F1"
+            style="width:34px;height:34px" class="dark:color-whit">
+        </lord-icon>
+        <a href="./dashclient.php"><span class="mx-4">Users</span></a>
+    </li>
+    <li
+        class="flex p-3 mb-15 w-3/4 h-14 transition-transform duration-200 ease-out transform-colors dark:hover:bg-purple-700  hover:scale-110 rounded mb-[10%]">
+        <script src="https://cdn.lordicon.com/lordicon-1.4.0.js"></script>
+        <b>
+            <lord-icon src="https://cdn.lordicon.com/bgebyztw.json" trigger="loop"
+                colors="primary:#6366F1,secondary:#6366F1" style="width:33px;height:33px;font-weight:700;">
+            </lord-icon>
+        </b>
+        <a href="./freelancer.php"><span class="mx-4">Freelancers</span></a>
+    </li>
+    <li
+        class="flex p-3 mb-15 w-3/4 h-14 transition-transform duration-200 ease-out transform-colors dark:hover:bg-purple-700  hover:scale-110 rounded mb-[10%]">
+        <script src="https://cdn.lordicon.com/lordicon.js"></script>
+        <lord-icon src="https://cdn.lordicon.com/bgitlnnj.json" trigger="loop"
+            colors="primary:#3080e8,secondary:#b4b4b4" style="width:33px;height:33px">
+        </lord-icon>
+        <a href="./category.php"><span class="mx-4">Category</span></a>
+    </li>
+    <li
+        class="flex p-3 mb-15 w-3/4 h-14 transition-transform duration-200 ease-out transform-colors dark:hover:bg-purple-700  hover:scale-110 rounded mb-[10%]">
+        <script src="https://cdn.lordicon.com/lordicon-1.4.0.js"></script>
+        <lord-icon src="https://cdn.lordicon.com/lecprnjb.json" trigger="loop" colors="primary:#6366F1"
+            style="width:33px;height:33px">
+        </lord-icon>
+        <a href="./projects.php"><span class="mx-4">Projects</span></a>
+    </li>
+    <li
+        class="flex p-3 mb-15 w-3/4 h-14 transition-transform duration-200 ease-out transform-colors dark:hover:bg-purple-700 hover:scale-110 rounded mb-[10%]">
+        <script src="https://cdn.lordicon.com/lordicon.js"></script>
+        <lord-icon src="https://cdn.lordicon.com/ayhtotha.json" trigger="loop" colors="primary:#3080e8"
+            style="width:33px;height:33px">
+        </lord-icon>
+        <a href="./testimonial.php"><span class="mx-4">testimonial</span></a>
+    </li>
+</ul>
+<hr class="mb-[10%]">
+</hr>
+<ul class="ml-[2%] text-lg">
+    <li
+        class="flex p-3 mb-15 w-3/4 h-14 transition-transform duration-200 ease-out transform-colors dark:hover:bg-purple-700  hover:scale-110 rounded mb-[10%]">
+        <svg xmlns="http://www.w3.org/2000/svg" width="25" height="27" viewBox="0 0 25 27" fill="none">
+            <path
+                d="M9.021 13.1182L11.021 15.333L15.521 10.3496M20.021 13.6719C20.021 19.1076 14.667 23.0611 12.719 24.3196C12.4976 24.4626 12.3869 24.5342 12.2307 24.5713C12.1094 24.6001 11.9326 24.6001 11.8113 24.5713C11.6551 24.5342 11.5444 24.4626 11.323 24.3196C9.37496 23.0611 4.021 19.1076 4.021 13.6719V8.37575C4.021 7.49036 4.021 7.04766 4.15176 6.66712C4.26727 6.33094 4.45498 6.03098 4.69865 5.79317C4.97449 5.52396 5.34879 5.36852 6.0974 5.05764L11.4592 2.83097C11.6671 2.74464 11.771 2.70147 11.878 2.68436C11.9728 2.66918 12.0692 2.66918 12.164 2.68436C12.271 2.70147 12.3749 2.74464 12.5828 2.83097L17.9446 5.05764C18.6932 5.36852 19.0675 5.52396 19.3433 5.79317C19.587 6.03098 19.7747 6.33094 19.8902 6.66712C20.021 7.04766 20.021 7.49036 20.021 8.37575V13.6719Z"
+                stroke="#6366F1" class="dark:stroke-white" stroke-width="2" stroke-linecap="round"
+                stroke-linejoin="round" />
+            <rect x="0.0209961" y="0.382812" width="24" height="26.5781" />
+        </svg>
+        <a href="#"><span class="mx-4">Security</span></a>
+    </li>
+    <li
+        class="flex p-3 mb-15 w-3/4 h-14 transition-transform duration-200 ease-out transform-colors dark:hover:bg-purple-700  hover:scale-110 rounded mb-[10%]">
+        <script src="https://cdn.lordicon.com/lordicon-1.4.0.js"></script>
+        <lord-icon src="https://cdn.lordicon.com/ojnjgkun.json" trigger="loop" delay="2000" colors="primary:#6366F1"
+            style="width:33px;height:33px">
+        </lord-icon>
+        <a href="#"><span class="mx-4">Help Center</span></a>
+    </li>
+    <li>
+        <button type="button" id="checkbox-wrapper">
+            <label class="text-[17px] relative inline-block w-[3.5em] h-[2em]">
+                <input type="checkbox" class="invisible focus:shadow-[0_0_1px_#007bff]">
+                <span
+                    class="slider bg-white absolute cursor-pointer border transition duration-300 rounded-[30px] border-solid border-[#adb5bd] inset-0 before:absolute before:content-[''] before:h-[1.4em] before:w-[1.4em] before:bg-[#FE8D4D] before:transition before:duration-300 before:rounded-[20px] before:left-[0.27em] before:bottom-[0.25em]"></span>
+            </label>
+        </button>
+    </li>
+</ul>
+<div class="flex flex-row ml-[4%]" style="margin-top:0.75rem;">
+    <div class="rounded-[50%] bg-slate-400 mr-[7%]">
+        <img class="rounded-[50%]" src="../../images/young man in formalwear standing with hands on hip.svg"
+            alt="freelencer">
+    </div>
+    <div class="text-black dark:text-white">
+        <h1 class="text-2xl">Ali Riaz</h1>
+        <p>Web Developer</p>
+    </div>
+</div>
+</div>
